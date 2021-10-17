c6bb826fcfb082df26d726578c9dfa4faab72e84
SethThomaspowered <athomas.seth@gmail.com>
2021-10-17 17:20:17 -0400

    fixed typos

--- jobs/app.py
+++ jobs/app.py
@@ -51,8 +51,9 @@ def employer(employer_id):
 def review(employer_id):
     if request.method == 'POST':
         review = request.form['review']
-        rating = request.form['title']
-        rating = request.form['status']
+        rating = request.form['rating']
+        title = request.form['title']
+        status= request.form['status']
 
         date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
